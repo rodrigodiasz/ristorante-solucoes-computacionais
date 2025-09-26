1f5a830900446ab61e63673db8aaeee21d007640
Rodrigo Oliveira <coderodrigo07@gmail.com>
2025-09-26 20:55:34 -0300

    fix ts error

--- back-end/src/controllers/user/CreateUserController.ts
+++ back-end/src/controllers/user/CreateUserController.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express";
 import { CreateUserService } from "../../services/user/CreateUserService";
 
 class CreateUserController {
-    async handle(req: Request, res: Response) {
-        const { name, email, password } = req.body;
+  async handle(req: Request, res: Response) {
+    const { name, email, password } = req.body;
 
-        const createUserService = new CreateUserService();
+    const createUserService = new CreateUserService();
 
     try {
       const user = await createUserService.execute({
@@ -14,7 +14,7 @@ class CreateUserController {
         password,
       });
       return res.json(user);
-    } catch (error) {
+    } catch (error: any) {
       return res.status(400).json({
         error: error.message,
       });
