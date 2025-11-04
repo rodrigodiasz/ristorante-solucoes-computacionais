30cd6fbd4ddeee1f79b3e0fdbe7b5cc872c3534c
Rodrigo Oliveira <coderodrigo07@gmail.com>
2025-11-03 22:57:34 -0300

    fix cors

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.USER_APP_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
